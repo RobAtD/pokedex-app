56b3b2fc95e690fcd5cd11b4bd16314b00af121b
RobAtD <r.ditler@devduck.de>
2024-06-14 16:40:09 +0200

    removes old example code and adds variable "pokemonList" to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,49 @@
-alert('Hello world');
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+// Create Object with a list of Pokemons
+let pokemonList = [];
+pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        type: ['Grass', 'Poison'],
+    },
+    {
+        name: 'Ivysaur',
+        height: 1,
+        type: ['Grass', 'Poison'],
+    },
+    {
+        name: 'Venusaur',
+        height: 2,
+        type: ['Grass', 'Poison'],
+    },
+    {
+        name: 'Charmander',
+        height: 0.6,
+        types: ['Fire'],
+    },
+    {
+        name: 'Charmeleon',
+        height: 1.1,
+        type: ['Fire'],
+    },
+    {
+        name: 'Charizard',
+        height: 1.7,
+        type: ['Fire', 'Flying'],
+    },
+    {
+        name: 'Squirtle',
+        height: 0.5,
+        types: ['Water'],
+    },
+    {
+        name: 'Wartortle',
+        height: 1,
+        types: ['Water'],
+    },
+    {
+        name: 'Blastoise',
+        height: 1.6,
+        types: ['Water'],
+    },
+];
