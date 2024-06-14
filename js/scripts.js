09b232c8502af1738f6167cbefedc8b7722fd67a
RobAtD <r.ditler@devduck.de>
2024-06-14 16:47:22 +0200

    removes unnecessary commas at scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,46 +4,46 @@ pokemonList = [
     {
         name: 'Bulbasaur',
         height: 0.7,
-        type: ['Grass', 'Poison'],
+        type: ['Grass', 'Poison']
     },
     {
         name: 'Ivysaur',
         height: 1,
-        type: ['Grass', 'Poison'],
+        type: ['Grass', 'Poison']
     },
     {
         name: 'Venusaur',
         height: 2,
-        type: ['Grass', 'Poison'],
+        type: ['Grass', 'Poison']
     },
     {
         name: 'Charmander',
         height: 0.6,
-        types: ['Fire'],
+        types: ['Fire']
     },
     {
         name: 'Charmeleon',
         height: 1.1,
-        type: ['Fire'],
+        type: ['Fire']
     },
     {
         name: 'Charizard',
         height: 1.7,
-        type: ['Fire', 'Flying'],
+        type: ['Fire', 'Flying']
     },
     {
         name: 'Squirtle',
         height: 0.5,
-        types: ['Water'],
+        types: ['Water']
     },
     {
         name: 'Wartortle',
         height: 1,
-        types: ['Water'],
+        types: ['Water']
     },
     {
         name: 'Blastoise',
         height: 1.6,
-        types: ['Water'],
-    },
+        types: ['Water']
+    }
 ];
