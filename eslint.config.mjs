d05d1e671ae05d80335f40c69c314601eb069baa
RobAtD <r.ditler@devduck.de>
2024-08-02 14:36:21 +0200

    adds indentation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,21 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  {ignores: ["**/promise-polyfill.js"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: {
-    $: "writable",
-    ...globals.browser}}},
-  pluginJs.configs.recommended,
-  {rules: {
-    "camelcase": "error"
-  }},
+    { ignores: ['**/promise-polyfill.js'] },
+    { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
+    {
+        languageOptions: {
+            globals: {
+                $: 'writable',
+                ...globals.browser,
+            },
+        },
+    },
+    pluginJs.configs.recommended,
+    {
+        rules: {
+            camelcase: 'error',
+        },
+    },
 ];
